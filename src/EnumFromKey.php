46564fa785f3e857f3370ab3fc47c517f345897d
Dovydas Bukauskas <dovydasbukauskas@gmail.com>
2025-04-11 01:54:23 +0300

    Generalize data for tests

--- src/EnumFromKey.php
+++ src/EnumFromKey.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Dovydasbu\Enum;
 
+use RuntimeException;
+
 trait EnumFromKey
 {
     public static function fromKey(string $key, mixed $value): ?self
     {
         foreach (self::cases() as $case) {
-            if ($case->$key() === $value) {
+            $caseValue = $case->$key();
+
+            if ($caseValue === $value) {
                 return $case;
             }
         }
 
-        throw new \RuntimeException(sprintf(
+        throw new RuntimeException(sprintf(
             'Value [%s] not found for key [%s] for enum class [%s]',
             $value,
             $key,
--- tests/src/EnumComparisonTest.php
+++ tests/src/EnumComparisonTest.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace Dovydasbu\tests\src;
 
-use Dovydasbu\Tests\Data\Status;
+use Dovydasbu\Tests\Data\TestEnum;
 use PHPUnit\Framework\TestCase;
+use TypeError;
 
 class EnumComparisonTest extends TestCase
 {
     public function test_trait_compares_itself_correctly()
     {
-        self::assertTrue(Status::DRAFT->is(Status::DRAFT));
-        self::assertTrue(Status::ACTIVE->is(Status::ACTIVE));
-        self::assertTrue(Status::CANCELED->is(Status::CANCELED));
+        self::assertTrue(TestEnum::FIRST_KEY->is(TestEnum::FIRST_KEY));
+        self::assertTrue(TestEnum::SECOND_KEY->is(TestEnum::SECOND_KEY));
+        self::assertTrue(TestEnum::THIRD_KEY->is(TestEnum::THIRD_KEY));
     }
 
     public function test_trait_compares_itself_incorrectly()
     {
-        self::assertFalse(Status::DRAFT->is(Status::ACTIVE));
-        self::assertFalse(Status::ACTIVE->is(Status::CANCELED));
-        self::assertFalse(Status::CANCELED->is(Status::ACTIVE));
+        self::assertFalse(TestEnum::FIRST_KEY->is(TestEnum::SECOND_KEY));
+        self::assertFalse(TestEnum::SECOND_KEY->is(TestEnum::THIRD_KEY));
+        self::assertFalse(TestEnum::THIRD_KEY->is(TestEnum::FIRST_KEY));
+    }
+
+    public function test_throws_error_if_string_is_presented(): void
+    {
+        $this->expectException(TypeError::class);
+
+        TestEnum::FIRST_KEY->is('fake-enum');
+    }
+
+    public function test_throws_error_if_int_is_presented(): void
+    {
+        $this->expectException(TypeError::class);
+
+        TestEnum::FIRST_KEY->is(12);
     }
 }
